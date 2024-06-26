a63767af016bcae2262430212046630864e20107
Tejas9aug <122152847+Tejas9aug@users.noreply.github.com>
2024-06-26 13:29:50 +0530

    context

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,16 @@
-import { useContext } from "react"
+import { useContext, useEffect } from "react"
 import React from 'react'
 import noteContext from "../context/notes/noteContext"
 
 
 function About() {
   const a =useContext(noteContext)
+  useEffect(() => {
+    a.update()
+  },[])
   return (
     <div>
-     This is about {a.name}
+     This is about {a.state.name} and in {a.state.class}
     </div>
   )
 }
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,12 +1,28 @@
+import { useState } from "react";
 import noteContext from "./noteContext";
 
 const NoteState = (props)=>{
-    const state ={
+    const s1 ={
         "name":"Teja",
         "class":"IT"
     }
+
+    const [state,setState] = useState(s1);
+    const update = () =>{
+        setTimeout(()=>{
+            setState({
+                "name":"Tejas",
+                 "class":"IIT"
+            }
+
+            )
+
+        },1000
+
+        )
+    }
     return(
-        <noteContext.Provider value={state}>
+        <noteContext.Provider value={{state,update}}>
                {props.children}
         </noteContext.Provider>
     )
