b1d0ece37329d5f5acab2b3a388b4f667580fd27
Tejas9aug <122152847+Tejas9aug@users.noreply.github.com>
2024-06-28 22:29:58 +0530

    notes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,31 @@
 import React from 'react'
 
+import Notes from './Notes';
 function Home() {
+  
   return (
     <div>
-      HOME Page
+      
+      <div className="container my-3">
+      <h2>Add a Form</h2>
+      <form className='my-3'>
+  <div className="form-group mb-3">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+    <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
+  <div className="form-group mb-3">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"/>
+  </div>
+  <div className="form-check mb-3 ">
+    <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
+    <label className="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" className="btn btn-primary">Submit</button>
+</form>
+</div>
+   <Notes/>
     </div>
   )
 }
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -2,27 +2,83 @@ import { useState } from "react";
 import noteContext from "./noteContext";
 
 const NoteState = (props)=>{
-    const s1 ={
-        "name":"Teja",
-        "class":"IT"
-    }
-
-    const [state,setState] = useState(s1);
-    const update = () =>{
-        setTimeout(()=>{
-            setState({
-                "name":"Tejas",
-                 "class":"IIT"
-            }
-
-            )
-
-        },1000
-
-        )
-    }
+  const notesinitial = [
+  {
+    "user": "666c52b73fc4328a2df0a052",
+    "title": "codeforces",
+    "description": "specialist on codeforces.",
+    "tag": "General",
+    "_id": "667ec5a1935aa8cef85bcea3",
+    "date": "2024-06-28T14:16:01.070Z",
+    "__v": 0
+  },
+  {
+    "user": "666c52b73fc4328a2df0a052",
+    "title": "codeforces",
+    "description": "specialist on codeforces.",
+    "tag": "General",
+    "_id": "667ec5a1935aa8cef85bcea3",
+    "date": "2024-06-28T14:16:01.070Z",
+    "__v": 0
+  },
+  {
+    "user": "666c52b73fc4328a2df0a052",
+    "title": "codeforces",
+    "description": "specialist on codeforces.",
+    "tag": "General",
+    "_id": "667ec5a1935aa8cef85bcea3",
+    "date": "2024-06-28T14:16:01.070Z",
+    "__v": 0
+  },
+  {
+    "user": "666c52b73fc4328a2df0a052",
+    "title": "codeforces",
+    "description": "specialist on codeforces.",
+    "tag": "General",
+    "_id": "667ec5a1935aa8cef85bcea3",
+    "date": "2024-06-28T14:16:01.070Z",
+    "__v": 0
+  },
+  {
+    "user": "666c52b73fc4328a2df0a052",
+    "title": "codeforces",
+    "description": "specialist on codeforces.",
+    "tag": "General",
+    "_id": "667ec5a1935aa8cef85bcea3",
+    "date": "2024-06-28T14:16:01.070Z",
+    "__v": 0
+  },
+  {
+    "user": "666c52b73fc4328a2df0a052",
+    "title": "codeforces",
+    "description": "specialist on codeforces.",
+    "tag": "General",
+    "_id": "667ec5a1935aa8cef85bcea3",
+    "date": "2024-06-28T14:16:01.070Z",
+    "__v": 0
+  },
+  {
+    "user": "666c52b73fc4328a2df0a052",
+    "title": "codeforces",
+    "description": "specialist on codeforces.",
+    "tag": "General",
+    "_id": "667ec5a1935aa8cef85bcea3",
+    "date": "2024-06-28T14:16:01.070Z",
+    "__v": 0
+  },
+  {
+    "user": "666c52b73fc4328a2df0a052",
+    "title": "codeforces",
+    "description": "specialist on codeforces.",
+    "tag": "General",
+    "_id": "667ec5a1935aa8cef85bcea3",
+    "date": "2024-06-28T14:16:01.070Z",
+    "__v": 0
+  }
+]
+const [notes,setnotes] = useState(notesinitial);
     return(
-        <noteContext.Provider value={{state,update}}>
+        <noteContext.Provider value={{notes}}>
                {props.children}
         </noteContext.Provider>
     )
