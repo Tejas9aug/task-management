29df9d67c8439542ecb28579aa4d5e16504b577e
Tejas9aug <122152847+Tejas9aug@users.noreply.github.com>
2024-07-03 10:05:28 +0530

    navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import About from "./components/About";
-import Home from "./components/Home";
+import {Home} from "./components/Home";
 import Navbar from "./components/Navbar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NoteState from './context/notes/NoteState';
-import Alert from "./components/ALert";
+import Alert from "./components/Alert";
+import Login from "./components/Login";
+import Signup from "./components/SignUp";
 
 
 function App() {
@@ -18,6 +20,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/about" element={<About/>}></Route>
+          <Route path="/login" element ={<Login/>}></Route>
+          <Route path="/signup" element = {<Signup/>}/>
         </Routes>
         </div>
 
